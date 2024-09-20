7790f3a853ddb1b858b1c8d32fb7004e3c3fab6e
deepcu-it <deepghosal445@gmail.com>
2024-09-20 14:50:43 +0530

    data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,7 +3,7 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
 from dataclasses import dataclass
 
 
@@ -38,6 +38,9 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e,sys)
+        
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data_path,test_data_path = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data_path,test_data_path)
